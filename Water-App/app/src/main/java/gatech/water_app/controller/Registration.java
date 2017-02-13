b8a16aff5e90793c859c511650c14bf5a558224e
lealex262 <lealex262@gmail.com>
2017-02-13 14:32:10 -0500

    Created Registration Page and connect it to welcome page

--- Water-App/app/src/main/java/gatech/water_app/controller/Registration.java
+++ Water-App/app/src/main/java/gatech/water_app/controller/Registration.java
@@ -1,6 +1,5 @@
-package gatech.water_app;
+package gatech.water_app.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class welcomePage extends AppCompatActivity {
+import gatech.water_app.R;
+
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_page);
+        setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,11 +26,6 @@ public class welcomePage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    public void startLoginPage(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 
 }
---
+++ Water-App/app/src/main/java/gatech/water_app/controller/welcomePage.java
@@ -0,0 +1,43 @@
+package gatech.water_app.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import gatech.water_app.R;
+import gatech.water_app.controller.LoginActivity;
+
+public class welcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void startLoginPage(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void startRegistrationPage(View view) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+    }
+
+}
