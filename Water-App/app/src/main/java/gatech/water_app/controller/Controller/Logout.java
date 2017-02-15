bea41294ebd685743280b17730a2fc6e5bd31639
lealex262 <lealex262@gmail.com>
2017-02-15 10:15:24 -0500

    Created logout for M4

--- Water-App/app/src/main/java/gatech/water_app/controller/Controller/Logout.java
+++ Water-App/app/src/main/java/gatech/water_app/controller/Controller/Logout.java
@@ -1,5 +1,6 @@
 package gatech.water_app.controller.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,11 @@ public class Logout extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void loginout(View view) {
+        Intent intent = new Intent(this, welcomePage.class);
+        startActivity(intent);
     }
 
 }
