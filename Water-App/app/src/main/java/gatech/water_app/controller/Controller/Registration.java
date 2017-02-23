6d2f82e40bb07550fd8161b4c2b1fa96d040d011
vy <tvshuynh@yahoo.com>
2017-02-22 22:47:41 -0500

    Revised UI for registration

--- Water-App/app/src/main/java/gatech/water_app/controller/Controller/Registration.java
+++ Water-App/app/src/main/java/gatech/water_app/controller/Controller/Registration.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import gatech.water_app.R;
 
@@ -26,6 +27,10 @@ public class Registration extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+//        Button btnClick = (Button) findViewById(R.id.submitreg);
+//        btnClick.o
+
 
 
     }
