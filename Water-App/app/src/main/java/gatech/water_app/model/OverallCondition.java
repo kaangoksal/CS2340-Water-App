3bc57167e1aa9775dd9f149c5e8c44952b04c6ee
lealex262 <lealex262@gmail.com>
2017-02-28 14:41:02 -0500

    JavaDoc and getter & setter

--- Water-App/app/src/main/java/gatech/water_app/model/OverallCondition.java
+++ Water-App/app/src/main/java/gatech/water_app/model/OverallCondition.java
@@ -5,4 +5,39 @@ package gatech.water_app.model;
  */
 
 public enum OverallCondition {
+
+    Safe("Safe"),
+    Treatable("Treatable"),
+    Unsafe("Unsafe");
+
+    private String nameCondition;
+
+    /**
+     * Constructor
+     * @param nameCondition
+     */
+    OverallCondition(String nameCondition) {
+        this.nameCondition = nameCondition;
+    }
+
+    /**
+     * Getter
+     * @return
+     */
+    public String getNameCondition() {
+        return nameCondition;
+    }
+
+    /**
+     * Setter
+     * @param nameCondition
+     */
+    public void setNameCondition(String nameCondition) {
+        this.nameCondition = nameCondition;
+    }
+
+    @Override
+    public String toString() {
+        return nameCondition;
+    }
 }
--- Water-App/app/src/main/java/gatech/water_app/model/WaterPurityReport.java
+++ Water-App/app/src/main/java/gatech/water_app/model/WaterPurityReport.java
@@ -16,10 +16,68 @@ public class WaterPurityReport extends WaterSourceReport {
     private double virusPPM;
     private double  contaminantPPM;
 
+    /**
+     * Constructor
+     * @param dataTime
+     * @param reportNumber
+     * @param Reporter
+     * @param type
+     * @param overallCondition
+     * @param virusPPM
+     * @param contaminantPPM
+     */
     public WaterPurityReport(int dataTime, int reportNumber, String Reporter, WaterType type, OverallCondition overallCondition, double virusPPM, double contaminantPPM) {
        super(dataTime, reportNumber, Reporter, type, WaterCondition.NA);
         this.overallCondition = overallCondition;
         this.virusPPM = virusPPM;
         this.contaminantPPM = contaminantPPM;
+    }
+
+    /**
+     * Getter
+     * @return
+     */
+    public double getContaminantPPM() {
+        return contaminantPPM;
+    }
+
+    /**
+     * Setter
+     * @param contaminantPPM
+     */
+    public void setContaminantPPM(double contaminantPPM) {
+        this.contaminantPPM = contaminantPPM;
+    }
+
+    /**
+     * Getter
+     * @return
+     */
+    public double getVirusPPM() {
+        return virusPPM;
+    }
+
+    /**
+     * Setter
+     * @param virusPPM
+     */
+    public void setVirusPPM(double virusPPM) {
+        this.virusPPM = virusPPM;
+    }
+
+    /**
+     * Getter
+     * @return
+     */
+    public OverallCondition getOverallCondition() {
+        return overallCondition;
+    }
+
+    /**
+     * Setter
+     * @param overallCondition
+     */
+    public void setOverallCondition(OverallCondition overallCondition) {
+        this.overallCondition = overallCondition;
     }
 }
