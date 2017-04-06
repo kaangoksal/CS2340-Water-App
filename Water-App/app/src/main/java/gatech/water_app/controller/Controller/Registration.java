b6a8fa2c6fbcf8aa51ef7c0166d0f3688a25d6ec
John Ho <jhh65565@uga.edu>
2017-04-06 14:54:42 -0400

    Edited UI for welcome, login and registration, rest of the pages coming soon

--- Water-App/app/src/main/java/gatech/water_app/controller/Controller/Registration.java
+++ Water-App/app/src/main/java/gatech/water_app/controller/Controller/Registration.java
@@ -34,17 +34,7 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         username = (EditText) findViewById(R.id.usernameIn);
         password = (EditText) findViewById(R.id.passwordIn);
